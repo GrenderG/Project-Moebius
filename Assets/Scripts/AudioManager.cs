7080f65b824305af1dc337baa43e0f0f20841074
legomolina <legomolina@gmail.com>
2015-09-07 13:49:07 +0200

    Hecho el sistema de particulas de lluvia y comenzado el script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager {
+
+	public void PlaySoundOnCamera(string AudioName, AudioClip Clip) {
+
+		AudioClip Sound;
+		if(Clip != null) {
+			Sound = Clip;
+		}
+		else {
+			Sound = Resources.Load<AudioClip>("Audio/" + AudioName);
+		}
+
+		GameObject MainCamera = GameObject.FindWithTag("MainCamera");
+		GameObject SoundObject = new GameObject("Audio" + Sound.name);
+
+		SoundObject.transform.position = MainCamera.transform.position;
+		SoundObject.transform.parent = MainCamera.transform;
+
+		AudioSource Source = SoundObject.AddComponent<AudioSource>();
+
+		Source.GetComponent<AudioSource>().clip = Sound;
+		Source.GetComponent<AudioSource>().Play();
+		UnityEngine.Object.Destroy(SoundObject, Sound.length);
+	}
+}
---
+++ Assets/Scripts/Enviroment/Rain.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rain : MonoBehaviour {
+	[Header("Rain variables (seconds)")]
+	public ParticleSystem RainParticles;
+	public AudioClip RainAudio;
+	public float RainDuration = 60;
+	public int HowMuchTimesInADayCanOccur = 1;
+	public float ProbabilityOfRain = 0.25f;
+	public float TimeBetweenRainAndRain = 0;
+	[Header("Thunder variables (seconds)")]
+	public bool Thunders = false;
+	public AudioClip ThundersAudio;
+	public float ProbabilityOfThundersInARain = 0.25f;
+	public float TimeBetweenThunderAndThunder = 0;
+	[Header("Ambient variables")]
+	[Range(0, 1)]public float AmbientIntensity = 0.7f;
+
+	private float actualTime = 0;
+	private AudioManager AudioManager;
+	
+	void Start () {
+	}
+
+	void Update () {
+		actualTime += Time.deltaTime;
+
+		if(Mathf.Floor(actualTime) >= RainDuration) {
+			//Stop Raining
+		}
+	
+	}
+}
