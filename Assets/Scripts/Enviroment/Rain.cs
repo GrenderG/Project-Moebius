8b579c8924f6c39bfb46a4784e006cfe37ad540a
legomolina <legomolina@gmail.com>
2015-09-08 21:21:05 +0200

    Subido lluvia

--- Assets/Scripts/Enviroment/Rain.cs
+++ Assets/Scripts/Enviroment/Rain.cs
@@ -3,15 +3,53 @@ using System.Collections;
 
 public class Rain : MonoBehaviour {
 
+	public float DayDuration;
+	[Header("Rain variables (seconds)")]
+	public ParticleSystem RainParticles;
+	public AudioClip RainAudio;
+	public float RainDuration = 60;
+	public int HowMuchTimesInADayCanOccur = 1;
+	[Range(0, 100)]public int ProbabilityOfRain = 0;
+	public float TimeBetweenRainAndRain = 0;
+	[Header("Thunder variables (seconds)")]
+	public bool Thunders = false;
+	public AudioClip ThundersAudio;
+	public float ProbabilityOfThundersInARain = 0.25f;
+	public float TimeBetweenThunderAndThunder = 0;
+	[Header("Ambient variables")]
+	[Range(0, 1)]public float AmbientIntensity = 0.7f;
+
 	private EnviromentController Enviroment;
 	private float actualTime = 0;
 	private AudioManager AudioManager;
 	private float dayDuration;
-	private DayNight daynight;
+	private float[] TimesRained;
 	private bool rainFinished = true;
 	private bool itRained = false;
 
 	void Start() {
+		TimesRained = new float[HowMuchTimesInADayCanOccur];
+	}
+
+	void Update() {
+		actualTime += Time.deltaTime;
+
+		print (actualTime);
+
+		if (Mathf.Floor(actualTime) >= DayDuration / 60) {
+			actualTime = 0;
+
+			for(int i = 0; i < TimesRained.Length; i ++) {
+				TimesRained[i] = 0;
+			}
+		}
+
+
+	}
+
+
+
+	/*void Start() {
 		dayDuration = daynight.GetDayDuration();
 	}
 	
@@ -25,9 +63,9 @@ public class Rain : MonoBehaviour {
 		print(actualTime);
 		print (itRained);
 
-		/*if(itRained) {
-			StopCoroutine("PlayRain");
-		}*/
+		//if(itRained) {
+		//	StopCoroutine("PlayRain");
+		//}
 
 		if(Random.Range(0, 100) < ProbabilityOfRain) { //llueve
 			if(itRained && Mathf.Floor(actualTime) >= TimeBetweenRainAndRain) {
@@ -53,5 +91,5 @@ public class Rain : MonoBehaviour {
 		actualTime = 0;
 		itRained = true;
 		yield return null;
-	}
+	}*/
 }
