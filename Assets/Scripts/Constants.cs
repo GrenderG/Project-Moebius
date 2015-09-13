8f4f47316eecabba4f5481bceef1c6aa3f28bd9e
legomolina <legomolina@gmail.com>
2015-09-13 19:30:20 +0200

    constants and player

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants {
+	public const string DAWNTIME = "DAWNTIME";
+	public const string DAYTIME = "DAYTIME";
+	public const string DUSKTIME = "DUSKTIME";
+	public const string NIGHTTIME = "NIGHTTIME";
+
+	public static readonly Color THUNDER_COLOR = new Color32(206, 222, 235, 0);
+}
---
+++ Assets/Scripts/textAnimatedMaterial.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textAnimatedMaterial : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		 
+	}
+}
